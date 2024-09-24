dacbfeaa9ccb28b1fd817c69ad33550d3decaf90
aimanshakeel <aimanshakeel1008@gmail.com>
2024-09-24 22:52:13 +0530

    Initial commit

---
+++ user-service/user-service/src/test/java/com/devorbit/linkedin/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devorbit.linkedin.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
